88facfa31992dff4c896e7b272d62977d48b27f4
MostlyWhat <mostlywhatstudios@gmail.com>
2023-07-07 22:15:27 +0700

    Icon Hotfix

--- src/components/modal/display-modal.tsx
+++ src/components/modal/display-modal.tsx
@@ -38,7 +38,7 @@ export function DisplayModal({ closeModal }: DisplayModalProps): JSX.Element {
                    border-light-border px-4 py-3 dark:border-dark-border'
       >
         <div className='grid grid-cols-[auto,1fr] gap-3'>
-          <UserAvatar src='/assets/twitter-avatar.jpg' alt='Endeavour' />
+          <UserAvatar src='/assets/avatar.jpg' alt='Endeavour' />
           <div>
             <div className='flex gap-1'>
               <UserName verified name='Endeavour' />
